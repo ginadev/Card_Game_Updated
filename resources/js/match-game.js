5d051a312e39f5b21ba6c63f35e5e16b3e7d8268
Gina <ginalg@icloud.com>
2021-02-22 12:25:44 +0000

    Adding updated card game

--- resources/js/match-game.js
+++ resources/js/match-game.js
@@ -43,14 +43,14 @@ MatchGame.generateCardValues = function () {
 
 MatchGame.renderCards = function(cardValues, $game) {
 	var colors = [
-	'hsl(25, 85%, 65%)',
-	'hsl(55, 85%, 65%)',
-	'hsl(90, 85%, 65%)',
-	'hsl(160, 85%, 65%)',
-	'hsl(220, 85%, 65%)',
-	'hsl(265, 85%, 65%)',
-	'hsl(310, 85%, 65%)',
-	'hsl(360, 85%, 65%)'
+	'url("https://www.magazinesdirect.com/images/covers/large-POM.jpg")',
+	'url("https://www.magazinesdirect.com/images/covers/large-CRK.jpg")',
+	'url("https://www.magazinesdirect.com/images/covers/large-PRG.jpg")',
+	'url("https://www.magazinesdirect.com/images/covers/large-TOF.jpg")',
+	'url("https://www.magazinesdirect.com/images/covers/large-EDG.jpg")',
+	'url("https://www.magazinesdirect.com/images/covers/large-RET.jpg")',
+	'url("https://www.magazinesdirect.com/images/covers/large-PCG.jpg")',
+	'url("https://www.magazinesdirect.com/images/covers/large-TTT.jpg")'
 	]
 	
 	$game.empty();
@@ -79,15 +79,13 @@ MatchGame.renderCards = function(cardValues, $game) {
   Flips over a given card and checks to see if two cards are flipped over.
   Updates styles on flipped cards depending whether they are a match or not.
  */
+var winnerCounter = 0;	
 
 MatchGame.flipCard = function($card, $game) {
 	
 
-	if ($card.data('isFlipped').length === 16){
-		document.getElementbyID('secret').style.display = 'block';
-	}
 
-	$card.css('background-color', $card.data('color')).text($card.data('value')).data('isFlipped', true);
+	$card.css('background-image', $card.data('color')).text($card.data('value')).data('isFlipped', true);
 
 	var flippedCards = $game.data('flippedCards');
 	flippedCards.push($card);
@@ -96,24 +94,31 @@ MatchGame.flipCard = function($card, $game) {
 		if (flippedCards[0].data('value') === flippedCards[1].data('value')) {
 			var matchCss = {
 				backgroundColor: 'rgb(153, 153, 153)',
-				color: 'rgb(204, 204, 204)'
-			}
+				color: 'rgb(204, 204, 204)',
+				}
 			flippedCards[0].css(matchCss);
 			flippedCards[1].css(matchCss);
+			winnerCounter++;
 		} else{
 			var card1 = flippedCards[0];
 			var card2 = flippedCards[1];
 			window.setTimeout(function(){
-				card1.css('background-color', 'rgb(32, 64, 86').text('').data('isFlipped', false);
-			card2.css('background-color', 'rgb(32, 64, 86').text('').data('isFlipped', false);
+				card1.css('background-image', '').text('').data('isFlipped', false);
+			card2.css('background-image', '').text('').data('isFlipped', false);
 		}, 1000)
 			
-		}	
+		};	
+		
+
+		if (winnerCounter === 8){
+		console.log('success!'),
+		document.getElementById('secret').style.display = 'block'
+		};
+		
 
 	$game.data('flippedCards', []);
 	};
 }
-
 
 
 
